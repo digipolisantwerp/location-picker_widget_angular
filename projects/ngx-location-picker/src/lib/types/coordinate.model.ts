b8a73c0a589c4f5f29d38490f36af85b02b1e5a1
Florian Ramakers <florian.ramakers@continuous.be>
2019-11-14 08:12:49 +0100

    added comments explaining different keys

--- projects/ngx-location-picker/src/lib/types/coordinate.model.ts
+++ projects/ngx-location-picker/src/lib/types/coordinate.model.ts
@@ -2,8 +2,17 @@ import {LocationModel} from './location.model';
 import {AddressModel, LatLngModel} from './address.model';
 
 export interface CoordinateModel {
-    label?: string;
-    location?: LocationModel;
-    address?: AddressModel;
-    actualLocation?: LatLngModel;
+  label?: string;
+  /**
+   * Found location (park, poi, ...)
+   */
+  location?: LocationModel;
+  /**
+   * Found address
+   */
+  address?: AddressModel;
+  /**
+   * The coordinates used for determining a location or address by reverse lookup.
+   */
+  actualLocation?: LatLngModel;
 }
